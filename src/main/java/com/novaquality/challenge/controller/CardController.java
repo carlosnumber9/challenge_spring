a50910245e2d44570a431959db3a6e523860be2d
carlosnumber9 <charlie_theboss023@hotmail.com>
2020-02-24 00:12:15 +0100

    Implementation for best cards retrieval endpoint method

--- src/main/java/com/novaquality/challenge/controller/CardController.java
+++ src/main/java/com/novaquality/challenge/controller/CardController.java
@@ -1,5 +1,6 @@
 package com.novaquality.challenge.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.novaquality.challenge.service.CardService;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.novaquality.challenge.persistence.model.Card;
@@ -15,6 +15,8 @@ import com.novaquality.challenge.persistence.model.Card;
 @RestController
 @RequestMapping("cards")
 public class CardController {
+
+	private final Integer BEST_CARDS_LIST_LIMIT = 5;
 
 	@Autowired
 	private CardService cardService;
@@ -31,10 +33,18 @@ public class CardController {
 		return ResponseEntity.ok().build();
 	}
 
-	// TODO añadir path al endpoint
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Card>> getBestDeck() {
-		// TODO implementar endpoint
-		return ResponseEntity.notFound().build();
+
+		List<Card> bestCards = new ArrayList<>();
+
+		try {
+			bestCards = cardService.getBestCards(BEST_CARDS_LIST_LIMIT);
+		}
+		catch (IllegalStateException e) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(bestCards);
 	}
 
 	// TODO añadir path al endpoint

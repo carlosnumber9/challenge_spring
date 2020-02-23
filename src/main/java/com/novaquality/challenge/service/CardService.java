18743e10a502fe0610ea6abbc06afe78160fd153
carlosnumber9 <charlie_theboss023@hotmail.com>
2020-02-23 02:57:40 +0100

    First implementation for all cards score calculation

--- src/main/java/com/novaquality/challenge/service/CardService.java
+++ src/main/java/com/novaquality/challenge/service/CardService.java
@@ -1,4 +1,9 @@
 package com.novaquality.challenge.service;
 
+import com.novaquality.challenge.persistence.model.Card;
+
 public interface CardService {
+
+    public Integer calculateScore(Card card);
+
 }
--- src/main/java/com/novaquality/challenge/service/impl/CardServiceImpl.java
+++ src/main/java/com/novaquality/challenge/service/impl/CardServiceImpl.java
@@ -1,4 +1,35 @@
 package com.novaquality.challenge.service.impl;
 
-public class CardServiceImpl {
+import com.novaquality.challenge.persistence.model.Card;
+import com.novaquality.challenge.persistence.repository.impl.InMemoryCardRepository;
+import com.novaquality.challenge.service.CardService;
+
+import java.util.List;
+
+public class CardServiceImpl implements CardService {
+
+    private InMemoryCardRepository cardRepository;
+
+    /**
+     * Calculates score and sets it to all cards in repository
+     */
+    public void setScoreForAllCards() {
+        List<Card> allCards = cardRepository.findAll();
+        for(Card card : allCards) {
+            card.setScore(calculateScore(card));
+        }
+    }
+
+    /**
+     * Gets the score for one card.
+     * @param card Object to calculate score from
+     * @return Score for card
+     */
+    @Override
+    public Integer calculateScore(Card card) {
+        return card.getRareness().getValue() +
+                card.getCategory().getValue() +
+                card.getCost() +
+                card.getPower();
+    }
 }

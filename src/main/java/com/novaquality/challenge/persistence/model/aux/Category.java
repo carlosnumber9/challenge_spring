2e4a30499042e5f2932c179225ca0702f0b44242
carlosnumber9 <charlie_theboss023@hotmail.com>
2020-02-23 02:17:34 +0100

    Removing files

--- src/main/java/com/novaquality/challenge/persistence/model/aux/Category.java
+++
@@ -1,9 +0,0 @@
-package com.novaquality.challenge.persistence.model.aux;
-
-/** Representación de los posibles tipos de carta */
-public enum Category {
-	MANA,
-	MONSTER,
-	BLESSING,
-	CURSE
-}
--- src/main/java/com/novaquality/challenge/persistence/model/aux/Rareness.java
+++
@@ -1,9 +0,0 @@
-package com.novaquality.challenge.persistence.model.aux;
-
-/** Representación de la rareza de una carta */
-public enum Rareness {
-	COMMON,
-	RARE,
-	VERY_RARE,
-	LIMITED_EDITION
-}
